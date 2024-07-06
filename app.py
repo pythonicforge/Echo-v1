5ace86b0c2671d888127b086ac52c8c1b82abfc0
Hardik <168327453+pythonicforge@users.noreply.github.com>
2024-07-07 00:34:50 +0530

    Update app.py 2

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from sentence_transformers import SentenceTransformer, util
 import numpy as np
 
 app = Flask(__name__)
+CORS(app)
 
 # Load the model and data
 model = SentenceTransformer('paraphrase-MiniLM-L6-v2')
